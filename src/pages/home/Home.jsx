3d4741dc59ec4317cc4cb3e6b72c309bc6f63c35
roushan-sheik <roushansheik@gmail.com>
2024-07-09 21:46:46 +0600

    done: final project

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="bg_pri min-h-screen">Home</div>;
+  return (
+    <div className="bg_pri min-h-screen">
+      <div className=" flex justify-center pt-10 items-center">
+        <h2 className="text-4xl text-white font-bold">
+          Task Management Full Stack App
+        </h2>
+      </div>
+      <div className=" flex justify-center pt-6 items-center">
+        <h2 className="text-2xl text-white font-bold">
+          Admin Can Manage All the tasks
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
